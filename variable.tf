f7982740f3eb1f8a339bd2bcffe30044ce00d312
Rishabh Patel <rishabh.patel@nagarro.com>
2023-06-17 12:14:20 +0000

    Updated variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,23 @@
 variable "url" {
   type = string
+  sensitive = true
 }
+
 variable "token" {
   type = string
+  sensitive = true
 }
 variable "client_id" {
   type = string
+  sensitive = true
 }
 variable "client_secret" {
   type = string
+  sensitive = true
 }
 variable "tenant_id" {
   type = string
+  sensitive = true
 }
 
 
